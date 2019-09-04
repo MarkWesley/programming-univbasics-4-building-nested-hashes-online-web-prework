d4f53822d7271da102db4f28d5c2302296569f0b
Mark Stone <markwstone85@gmail.com>
2019-09-04 00:53:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # require_relative 'spec_helper'
 # require_relative '../intro_to_ruby_hashes_lab.rb'
 
@@ -138,6 +140,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+ binding.pry 
   {railroads: 
     {pieces: 4,
      rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
